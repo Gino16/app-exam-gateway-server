4609fbbfe04916c3e1f7f6db0b921e5f1eb41cc8
Gino Ascencio <gino.francisco@hotmail.com>
2022-08-28 11:07:04 -0500

    init gateway server

---
+++ src/test/java/com/exams/microservices/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exams.microservices.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
